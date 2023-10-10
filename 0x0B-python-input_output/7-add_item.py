f9d861173a77818be356cfe7a407083e6115c4df
amxsupport <itmasterdesigne@gmail.com>
2023-10-10 12:07:47 +0000

    update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""
-adds all arguments to a Python list, and then save them to a file
-"""
+# 7-add_item.py
+
+"""adds all arguments to a Python list, and then save them to a file."""
+
 import sys
 
 
@@ -9,6 +10,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 open("add_item.json", "a")
+
 try:
     l = load_from_json_file("add_item.json")
 except ValueError:
